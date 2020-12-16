baff3c0840fe06fa41883530765e3c9c8ef2f52f
Bruce Dickey <brucedickey@email.com>
2020-12-15 20:40:09 -0700

    Add favicon and soruce inages. Update footer. Add overall nav bars. Add home and about routes. Add About page. Update tasks and notes.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,14 @@
 
 class PagesController < ApplicationController
   def home
-    # Empty, so by default by convention expects a
-    # home.html.erb in app/views/pages/.
-    redirect_to articles_path if logged_in?
+    # Empty, so by default by convention expects a home.html.erb in app/views/pages/.
+
+    # TODO: Display this (from elsewhere) if in the Blog subapp...
+    #redirect_to articles_path if logged_in?
+
+    render "home"
   end
 
   def about; end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,30 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   root "pages#home"
+
+  get "/", to: "pages#home"
   get "home", to: "pages#home"
   get "about", to: "pages#about"
 
-  # For enabling only some of the REST routes for articles (list them)
-  # resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  namespace :blog do
+    #     Prefix Verb   URI Pattern                Controller#Action
+    # admin_users GET    /admin/users(.:format)     admin/users#index
+    #             POST   /admin/users(.:format)     admin/users#create
+    # admin_user  GET    /admin/users/:id(.:format) admin/users#show
+    #             PATCH  /admin/users/:id(.:format) admin/users#update
+    #             PUT    /admin/users/:id(.:format) admin/users#update
+    #             DELETE /admin/users/:id(.:format) admin/users#destroy
 
-  # For enabling all REST routes for articles
-  resources :articles
+    # For enabling only some of the REST routes for articles (list them)
+    # resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+
+    # For enabling all REST routes for articles
+    resources :articles
+
+    resources :categories, except: [:destroy]
+  end
 
   get "signup", to: "users#new" # Signup form
   # post 'users', to: 'users#create'
@@ -20,5 +35,4 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
-  resources :categories, except: [:destroy]
 end
