8aeebb3f6add3f09e331b0a6087af22758b3c64f
Bruce Dickey <brucedickey@email.com>
2021-01-01 19:06:35 -0700

    Update for Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,12 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.url = 'wss://redistogo-encircled-91756.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://redistogo-encircled-91756.heroku.com', 'http:\/\/redistogo-encircled-91756.heroku.*/' ]
+
+  # config.action_cable.url = 'wss://redistogo-encircled-91756.herokuapp.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'https://redistogo-encircled-91756.heroku.com', 'http:\/\/redistogo-encircled-91756.heroku.*/' ]
+
+  config.action_cable.url = 'wss://poly-app-bd.herokuapp.com/cable'
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
